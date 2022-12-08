eddb70625db331af23f223871c0f8f1d6c6baa6c
Ted L <dstrike@gmail.com>
2022-12-08 09:42:26 -0500

    Alpha 4
    Only check already submitted every 15 min

--- AdventOfCodeSupport/AdventClient.cs
+++ AdventOfCodeSupport/AdventClient.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using Microsoft.Extensions.Configuration;
 
@@ -7,12 +8,36 @@ namespace AdventOfCodeSupport;
 
 internal class AdventClient
 {
-    public record AdventAnswers(string? Part1, string? Part2);
-    
+    public class AdventAnswers
+    {
+        public string? Part1 { get; set; }
+        public string? Part2 { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+
     private static HttpClient? _client;
 
     private readonly Exception _badClient =
         new("Cannot download inputs/submit answers, user secret \"session\" has not been set.");
+
+    private static Dictionary<string, AdventAnswers>? _savedAnswers;
+    private static Dictionary<string, AdventAnswers> SavedAnswers
+    {
+        get
+        {
+            if (_savedAnswers is not null) return _savedAnswers;
+            if (!File.Exists("../../../Saved.json"))
+            {
+                _savedAnswers = new Dictionary<string, AdventAnswers>();
+                return _savedAnswers;
+            }
+
+            var text = File.ReadAllText("../../../Saved.json");
+            _savedAnswers = JsonSerializer.Deserialize<Dictionary<string, AdventAnswers>>(text)
+                            ?? new Dictionary<string, AdventAnswers>();
+            return _savedAnswers;
+        }
+    }
 
     public AdventClient()
     {
@@ -26,7 +51,7 @@ internal class AdventClient
         var handler = new HttpClientHandler { UseCookies = false };
         _client = new HttpClient(handler) { BaseAddress = new Uri("https://adventofcode.com/") };
 
-        var version = new ProductInfoHeaderValue("AdventOfCodeSupport", "2.0.0-alpha.3");
+        var version = new ProductInfoHeaderValue("AdventOfCodeSupport", "2.0.0-alpha.4");
         var comment = new ProductInfoHeaderValue("(+nuget.org/packages/AdventOfCodeSupport by @Zaneris)");
         _client.DefaultRequestHeaders.UserAgent.Add(version);
         _client.DefaultRequestHeaders.UserAgent.Add(comment);
@@ -49,23 +74,47 @@ internal class AdventClient
         await Task.Delay(1000); // Rate limit input downloads.
     }
 
+    private static async Task SaveAnswersAsync()
+    {
+        var text = JsonSerializer.Serialize(SavedAnswers);
+        await File.WriteAllTextAsync("../../../Saved.json", text);
+    }
+    
     public async Task<AdventAnswers> DownloadAnswersAsync(IAoC day)
     {
+        if (SavedAnswers.TryGetValue($"{day.Year}-{day.Day}", out var saved))
+        {
+            if (saved.Part2 is not null) return saved;
+            var diff = DateTime.UtcNow - saved.Timestamp;
+            
+            // Rate limit checking for new answers to every 15 minutes.
+            if (diff <= new TimeSpan(0, 15, 0)) return saved;
+        }
         if (_client is null) throw _badClient;
         var regex = new Regex(@"Your puzzle answer was <code>(.+)<\/code>");
-        Console.WriteLine($"Downloading answers {day.Year}-{day.Day}...");
+        Console.WriteLine($"Downloading your already submitted answers {day.Year}-{day.Day}...");
         var result = await _client.GetAsync($"{day.Year}/day/{day.Day}");
         if (!result.IsSuccessStatusCode)
             throw new Exception($"Downloading answers {day.Year}-{day.Day} failed. {result.ReasonPhrase}");
         var html = await result.Content.ReadAsStringAsync();
         await Task.Delay(1000); // Rate limit.
         var matches = regex.Matches(html);
-        return matches.Count switch
+        var answer = matches.Count switch
         {
-            1 => new AdventAnswers(matches[0].Groups[1].Value, null),
-            2 => new AdventAnswers(matches[0].Groups[1].Value, matches[1].Groups[1].Value),
-            _ => new AdventAnswers(null, null)
+            1 => new AdventAnswers
+            {
+                Part1 = matches[0].Groups[1].Value
+            },
+            2 => new AdventAnswers
+            {
+                Part1 = matches[0].Groups[1].Value, 
+                Part2 = matches[1].Groups[1].Value
+            },
+            _ => new AdventAnswers()
         };
+        SavedAnswers[$"{day.Year}-{day.Day}"] = answer;
+        await SaveAnswersAsync();
+        return answer;
     }
 
     public async Task<bool> SubmitAnswerAsync(IAoC day, int part, string submission)
@@ -89,6 +138,10 @@ internal class AdventClient
         if (regexGoldStar.IsMatch(html))
         {
             Console.WriteLine("You got a star!");
+            if (part == 1) SavedAnswers[$"{day.Year}-{day.Day}"] = new AdventAnswers { Part1 = submission };
+            else SavedAnswers[$"{day.Year}-{day.Day}"].Part2 = submission;
+            SavedAnswers[$"{day.Year}-{day.Day}"].Timestamp = DateTime.UtcNow;
+            await SaveAnswersAsync();
             return true;
         }
         if (regexIncorrect.IsMatch(html))
