f4a0172cb99ef8e1c951ca9d7cf44e28a71aa63e
Ted L <dstrike@gmail.com>
2023-12-06 08:51:03 -0500

    Fix download inputs

--- AdventOfCodeSupport/AdventClient.cs
+++ AdventOfCodeSupport/AdventClient.cs
@@ -96,7 +96,7 @@ internal partial class AdventClient
         var inputPattern = _adventSolutions.InputPattern;
         inputPattern = inputPattern.Replace("yyyy", $"{day.Year}");
         inputPattern = inputPattern.Replace("dd", $"{day.Day:D2}");
-        var path = $"../../../{inputPattern}";
+        var path = Path.Combine(AdventBase.ProjectRoot, inputPattern);
         if (File.Exists(path)) return;
         Console.WriteLine($"Downloading input {day.Year}-{day.Day}...");
         var directory = Path.GetDirectoryName(path);
